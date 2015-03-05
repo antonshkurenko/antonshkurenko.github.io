de0ab9983f642c4ba0c007fbdf9cdd01ceb311f9
CullyCross <elaugfein@gmail.com>
2015-03-05 16:41:27 +0200

    -Current release

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ window.onload = function() {
 
 	$('#bg').backstretch(arrayOfRandomBackgrounds, {duration: 3000, fade: 1500});
 
-	$('.target').blurjs({
-    	source: '#bg .backstretch img',
-    	radius: 7,
-   		overlay: 'rgba(255,255,255,0.4)'
+	// Tooltip only Text
+	$('.hovered').hover(function(){
+	    // Hover over code
+	    var title = $(this).attr('title');
+	    $(this).data('tipText', title).removeAttr('title');
+	    $('<p class="tooltip"></p>')
+	    	.text(title)
+	    	.appendTo('body')
+	    	.fadeIn('slow');
+	}, function() {
+        // Hover out code
+        $(this).attr('title', $(this).data('tipText'));
+        $('.tooltip').remove();
+	}).mousemove(function(e) {
+        var mousex = e.pageX + 15; //Get X coordinates
+        var mousey = e.pageY + 5; //Get Y coordinates
+        $('.tooltip')
+        	.css({ top: mousey, left: mousex })
 	});
 }
 
